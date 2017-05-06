6620c9a902252a2567fb74a00fbea87ca7e6bb93
kranthi <kumar_android@outlook.com>
2017-05-05 23:38:59 -0400

    added soap service classes using wsimport command

--- app/src/main/java/com/example/kranthikumarpolimetla/currencyconverter/CurrencyRate.java
+++ app/src/main/java/com/example/kranthikumarpolimetla/currencyconverter/CurrencyRate.java
@@ -1,8 +1,12 @@
 package com.example.kranthikumarpolimetla.currencyconverter;
 
+
+import net.webservicex.CurrencyConvertor;
+
 /**
  * Created by kranthikumarpolimetla on 4/30/17.
  */
 
 public class CurrencyRate {
+
 }
